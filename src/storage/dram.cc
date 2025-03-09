b87f192702f5a02be473c9b05b8b34f1dc7e4296
Siddarth-Suresh <65844402+Siddarth-Suresh@users.noreply.github.com>
2025-03-09 11:39:59 -0400

    Implement dram load

--- src/storage/dram.cc
+++ src/storage/dram.cc
@@ -38,4 +38,15 @@ Response Dram::write(Accessor accessor, signed int data, int address)
 	return r;
 }
 
-Response Dram::read(Accessor accessor, int address) { return WAIT; }
+Response Dram::read(Accessor accessor, int address, std::array<signed int, LINE_SIZE>& data) { 
+	Response r = WAIT;
+	if (this->requester == IDLE)
+		this->requester = accessor;
+	if (this->requester == accessor) {
+		if (this->wait_time == 0) {
+			this->do_read(data, address);
+			r = OK;
+		}
+	}
+	return r; 
+ }
